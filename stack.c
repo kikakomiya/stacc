39f833ca55d24a1993f004d1259fe846817f21d1
Samantha Rose <samfujisakidev@gmail.com>
2021-07-06 18:39:38 +0100

    add push support

--- stack.c
+++ stack.c
@@ -13,18 +13,27 @@ struct stacc
     int items[128];
 };
 
-void push()
+void push(struct stacc stack, int item)
 {
+    stack.items[stack.total] = item;
+    printf("%d pushed\n", item);
+    stack.total++;
 }
 
 int pop(struct stacc stack)
 {
     int item = stack.items[stack.total-1];
+    printf("%d removed\n", item);
     stack.total--;
     return item;
 }
 
 int main()
 {
+    struct stacc stack;
+    push(stack, 3);
+    push(stack, 7);
+    push(stack, 1423);
+    pop(stack);
     return 0;
 }
